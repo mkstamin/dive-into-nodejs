09f1dc655755c19ec96bac65394ffc4c88c7cfd7
mkstamin <mkstamincse@gamil.com>
2020-12-29 22:26:43 +0600

    work with bookmark.js

--- public/scripts/bookmarks.js
+++ public/scripts/bookmarks.js
@@ -1,3 +1,6 @@
 window.onload = function () {
     const bookmarks = document.getElementsByClassName('bookmark');
+    [...bookmarks].forEach((bookmark) => {
+        bookmark.style.cursor = 'pointer';
+    });
 };
